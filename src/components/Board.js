ac8d1f6dc4c98aae84d22a4765bdf935bb11db3d
hyein-rachel <hyein_lee@berkeley.edu>
2022-04-30 22:00:41 -0700

    hw4

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/Board.css';
 import Square from "./Square";
 
 function Board() {
 
-    const status = 'Next player: X';
+    const [square, setSquare] = useState(Array(9).fill(null));
+    const [xIsNext, setxIsNext] = useState(true);
 
-    function renderSquare(i) {
-        return <Square />;
+    function handleClick(i) {
+      const squares = square.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = xIsNext ? 'X' : 'O';
+      setSquare({square: squares});
+      setxIsNext({xIsNext: !xIsNext});
     }
 
-    return (  
+    const renderSquare = (i) => {
+       <Square value={square[i]} onClick={handleClick(i)}/>
+    }
+
+    const winner = calculateWinner(square);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    return (
         <div>
           <div className="board-row">
             {renderSquare(0)}
@@ -30,6 +49,26 @@ function Board() {
           <div className="status">{status}</div>
         </div>
     )
+
+    function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
+    }
 }
 
 export default Board;
